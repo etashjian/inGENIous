f5af83fb243fe73e6c169b4bc9ca041baf564534
Addison Floyd <acfloyd@wisc.edu>
2014-11-03 15:59:21 -0600

    added cost function, with dummy constants

--- analysis/analyze.pl
+++ analysis/analyze.pl
@@ -4,36 +4,64 @@ while(<>){
 	print $_;
 	/(\d+) (\d+)/;
 	#print $1;
-	push(@frames,$1);
+	push(@frames,$2);
 	#print $2;
-	push(@times,$2);
+	push(@times,$1);
 	$count++;
 }
-print @frames; print "\n";
-print @times; print "\n";
-$wait = 0;
+#print @frames; print "\n";
+#print @times; print "\n";
+$wait = 1;
 $rate = 1;
-for (my $i = 0; $i < 10; $i++){
+for (my $i = 1; $i < 100; $i+=.5){
 	#print $i;
 	$wait = $i;
-	for (my $j = .5; $j <= 3; $j+=.5){
+	for (my $j = .1; $j <= 10; $j+=.1){
 		#print " $j\n";
 		$rate = $j;
 		#print "$rate(x - $wait)\n";
 		$test = 0;
 		for (my $k = 0;$k < $count; $k++){
+			#if ($rate*($times[$k] - $wait) > $frames[$k]){
 			if ($rate*($times[$k] - $wait) > $frames[$k]){
 				#print "found one\n";
 				#print "$rate(x - $wait)\n";
 				#break;
 				$test = 1;
-				#last;
+				last;
 			}
 		}
 		if ($test == 1){
 			last;
 		}
-		print "$rate*(x - $wait),";
+		#print "$rate*(x - $wait),";
+		push(@rates,$rate);
+		push(@waits,$wait);
 	}
 }
+print "\n";
+# now pick best rate
+#print "\n"; print @rates;
+#print "\n"; print @waits;
+#print "Solutions: "; print scalar @rates; print "\n";
+$alpha = 1;
+$betta = 1;
+$solutions = scalar @rates;
+my $best = 0;
+my $which = 0;
+for (my $i = 0; $i < $solutions; $i++){
+	my $tempwait = $waits[$i];
+	my $temprate = $rates[$i];
+	#my $cost = $temprate / $tempwait;
+	my $cost = (exp (-$alpha*$tempwait))*(1 - exp (-$betta*$temprate));
+	#print "$cost,";
+	if ($cost > $best){
+		$best = $cost;
+		$which = $i;
+	}
+}
+print "\nCost: $best\n";
+print "Best Rate: $rates[$which]\n";
+print "Best Wait: $waits[$which]\n";
+print "$rates[$which]*(x - $waits[$which])";
 print "\n";
