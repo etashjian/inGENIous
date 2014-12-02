f305b697e29efe227c5308d42560302a97f9c600
Addison Floyd <acfloyd@wisc.edu>
2014-12-02 10:38:37 -0600

    changed analyze function to meet new project description

--- analysis/analyze.pl
+++ analysis/analyze.pl
@@ -14,66 +14,16 @@ while(<>){
 	$count++;
 	#$maxrate = $2 / $times[0];
 }
-#print "maxrate: $maxrate\n";
-#print "$times[0]\n";
-#print @frames; print "\n";
-#print @times; print "\n";
-my $wait = 1;
-my @waits;
-my $rate = 1;
-my @rates;
-#for (my $i = $times[0]; $i < 5; $i+=.1){
-for (my $i = 0; $i < 3; $i+=.1){
-	#print $i;
-	$wait = $i;
-	for (my $j = 1; $j <= 200; $j+=10){
-		#print " $j\n";
-		$rate = $j;
-		#print "$rate(x - $wait)\n";
-		my $test = 0;
-		for (my $k = 0;$k < $count; $k++){
-			#if ($rate*($times[$k] - $wait) > $frames[$k]){
-			if ($rate*($times[$k] - $wait) > $frames[$k]){
-				#print "found one\n";
-				#print "$rate(x - $wait)\n";
-				#break;
-				$test = 1;
-				last;
-			}
-		}
-		if ($test == 1){
-			last;
-		}
-		#print "$rate*(x - $wait),";
-		push(@rates,$rate);
-		push(@waits,$wait);
-	}
+
+use List::Util qw( min max);
+# Find maximum frame
+my $maxFrame = max @frames;
+print "max Frame #: $maxFrame\n";
+my $maxTime = max @times;
+print "max Time: $maxTime\n";
+for (my $j = 10; $j <= 100; $j+=10){
+	my $startTime = max ($maxTime - ($maxFrame/$j),0);
+	print "$j * (x-$startTime),";
 }
-#print "\n";
-# now pick best rate
-#print "\n"; print @rates;
-#print "\n"; print @waits;
-print "Solutions: "; print scalar @rates; print "\n";
-my $alpha = 10;
-my $betta = .001;
-my $solutions = scalar @rates;
-my $best = 0;
-my $which = 0;
-for (my $i = 0; $i < $solutions; $i++){
-	my $tempwait = $waits[$i];
-	my $temprate = $rates[$i];
-	#my $cost = $temprate / $tempwait;
-	my $cost = (exp (-$alpha*$tempwait))*(1 - exp (-$betta*$temprate));
-	#my $first = exp (-$alpha*$tempwait);
-	#my $last = 1-exp(-$betta*$temprate);
-	#print "$tempwait,$temprate,$first,$last,$cost\n";
-	if ($cost > $best){
-		$best = $cost;
-		$which = $i;
-	}
-}
-print "\nUser Satisfaction: $best\n";
-print "Best Rate: $rates[$which]\n";
-print "Best Wait: $waits[$which]\n";
-print "$rates[$which]*(x - $waits[$which])";
-print "\n";
+print"\n";
+
--- analysis/plot.p
+++ analysis/plot.p
@@ -11,15 +11,5 @@ set key l
 #set xr [0:6]
 set yr [0:1000]
 
-#plot "data1.test", (x-1), 0.5*(x-1), 1.5*(x-1)
-#plot "data1.test", 1.1*(x - 1)
-#plot "data1.test",0.5*(x - 1),1*(x - 1),0.5*(x - 2),1*(x - 2),0.5*(x - 3),1*(x - 3),0.5*(x - 4),1*(x - 4),1.5*(x - 4),0.5*(x - 5),1*(x - 5),1.5*(x - 5),2*(x - 5),0.5*(x - 6),1*(x - 6),1.5*(x - 6),2*(x - 6),2.5*(x - 6),0.5*(x - 7),1*(x - 7),1.5*(x - 7),2*(x - 7),2.5*(x - 7),3*(x - 7),0.5*(x - 8),1*(x - 8),1.5*(x - 8),2*(x - 8),2.5*(x - 8),3*(x - 8),3.5*(x - 8),4*(x - 8),4.5*(x - 8),5*(x - 8),0.5*(x - 9),1*(x - 9),1.5*(x - 9),2*(x - 9),2.5*(x - 9),3*(x - 9),3.5*(x - 9),4*(x - 9),4.5*(x - 9),5*(x - 9),5.5*(x - 9),6*(x - 9),6.5*(x - 9),7*(x - 9),7.5*(x - 9),8*(x - 9),8.5*(x - 9),9*(x - 9),9.5*(x - 9),10*(x - 9)
-plot "data2.test", 0.1*(x - 1)
-#plot "data3.test", 3*(x-1), 3.3*(x-1)
-#plot "temp", 1805*(x - 0.1)
-#plot "temp.data2", 111*(x-.2)
-#plot "temp.data", 84*(x-.2)
-#plot "temp.data", "temp.data2"
-#plot "example.data", 100*(x - 1)
-#plot "data/packets_100.queue_1.window_1.data","data/packets_100.queue_2.window_2.data","data/packets_100.queue_3.window_3.data"
+plot "non_blocking.5.data", 10 * (x-0),20 * (x-0),30 * (x-0),40 * (x-0),50 * (x-0),60 * (x-0),70 * (x-0),80 * (x-0),90 * (x-0),100 * (x-0)
 pause -1
