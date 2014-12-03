3b3b7925be7a7015c3ff609b2be76eb0d4c1d100
Addison Floyd <acfloyd@wisc.edu>
2014-12-03 17:42:36 -0600

    newest analysis

--- analysis/run.sh
+++ analysis/run.sh
@@ -21,19 +21,19 @@ perl analyze.pl data/B_VM3_C_MA | tee B.7.txt
 echo 'Scenario C'
 echo 'Link 1'
 perl analyze.pl data/C_S0_VM2_MA | tee C.1.txt
-#echo 'Link 2'
-#perl analyze.pl data/C_S1_VM2_MA | tee C.2.txt
-#echo 'Link 3'
-#perl analyze.pl data/C_S2_VM4_MA | tee C.3.txt
-#echo 'Link 4'
-#perl analyze.pl data/C_S3_VM4_MA | tee C.4.txt
+echo 'Link 2'
+perl analyze.pl data/C_S1_VM2_MA | tee C.2.txt
+echo 'Link 3'
+perl analyze.pl data/C_S2_VM4_MA | tee C.3.txt
+echo 'Link 4'
+perl analyze.pl data/C_S3_VM4_MA | tee C.4.txt
 
 echo 'Scenario D'
 echo 'Link 1'
 perl analyze.pl data/D_S0_VM2_MA | tee D.1.txt
-#echo 'Link 2'
-#perl analyze.pl data/D_S1_VM2_MA | tee D.2.txt
-#echo 'Link 3'
-#perl analyze.pl data/D_S2_VM4_MA | tee D.3.txt
-#echo 'Link 4'
-#perl analyze.pl data/D_S3_VM4_MA | tee D.4.txt
+echo 'Link 2'
+perl analyze.pl data/D_S1_VM2_MA | tee D.2.txt
+echo 'Link 3'
+perl analyze.pl data/D_S2_VM4_MA | tee D.3.txt
+echo 'Link 4'
+perl analyze.pl data/D_S3_VM4_MA | tee D.4.txt
